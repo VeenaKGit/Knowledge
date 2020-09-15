2e835fb6ec0913d6fcb849c4b5f39c7c2faf4d33
veena.kalburgi <veena.kalburgi@gmail.com>
2020-09-15 00:19:43 -0500

    updated api_demo.py to for how to test the api, more notes added to notebooks

--- Flask App/api_demo.py
+++ Flask App/api_demo.py
@@ -1,5 +1,10 @@
-# This is a stand alone API file.
+# This is a stand alone API file. This is how REST API   are written.
+# All logic between a  REST API and Flask application remains the same, only difference is that REST will not
+#  have UI files like static (.css etc) and templates(.html)
 # Run this file and use Postman tool to verify the request.
+# to send it through POSTMAN or Chrome POST add-on use the following data
+# header : Content-Type , Value: application/json
+# Body Raw : {"var1":"1", "var2":"2", "operator":"+"} and send as a POST request
 
 from flask import Flask, request, jsonify
 
@@ -15,6 +20,7 @@ def home():
 @app.route('/math', methods=['GET', 'POST'])
 def math():
     if request.json is not None:
+        print("Received -", request.json)
         op = request.json['operator']
         var1 = int(request.json['var1'])
         var2 = int(request.json['var2'])
@@ -29,6 +35,7 @@ def math():
         else:
             res = 'Operator Error'
     else:
+        print("Not Received -", request.json)
         res = 'Request sent cannot be understood by the server'
     return jsonify(res)
 
