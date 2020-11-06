e047e3dc5c9178fc5bfdb1c5923d1e7cb4b00c9c
veena.kalburgi <veena.kalburgi@gmail.com>
2020-11-06 15:48:37 -0600

    how to use postgresql rather than sqlite

--- Flask App/flaskblog/__init__.py
+++ Flask App/flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
@@ -7,12 +8,12 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
 
 from flaskblog import routes
 from flaskblog.models import User, Post
